da805910ebb6fbfbc06d39c670abb881565bb910
A.R.Sahasransu <a.r.sahasransu@gmail.com>
2025-07-01 18:33:49 +0200

    Added gen level info and best track info

--- PhysicsTools/Scouting/plugins/Run3ScoutingEGammaP4RegressTrainNtupliser.cc
+++ PhysicsTools/Scouting/plugins/Run3ScoutingEGammaP4RegressTrainNtupliser.cc
@@ -19,12 +19,17 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/Exception.h"
 #include "FWCore/Utilities/interface/InputTag.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 
+#include "DataFormats/Common/interface/ValueMap.h"
 #include "DataFormats/DetId/interface/DetId.h"
 #include "DataFormats/EcalDetId/interface/EBDetId.h"
 #include "DataFormats/EcalDetId/interface/EEDetId.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+#include "DataFormats/Math/interface/LorentzVector.h"
+#include "DataFormats/Math/interface/deltaR.h"
 #include "DataFormats/Scouting/interface/Run3ScoutingElectron.h"
 
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
@@ -46,9 +51,14 @@ private:
   float getHitEnergy(const DetId&, const std::unordered_map<int, float>&);
 
   edm::EDGetTokenT<std::vector<Run3ScoutingElectron>> electronToken_;
+  edm::EDGetTokenT<edm::ValueMap<int>> run3SctEle_bestTrkToken_;
+  edm::EDGetTokenT<std::vector<reco::GenParticle>> genParticleToken_;
 
   TFile* outputFile_;
   TTree* tree_;
+
+  const bool isMC_;
+  float gen_pt_, gen_eta_, gen_phi_;
 
   // Variables for branches
   float pt_, eta_, phi_, rawe_, pse_, ecorrerror_, sieie_, hoe_, eiso_, hiso_, r9_, smin_, smaj_;
@@ -61,6 +71,10 @@ private:
   static constexpr unsigned int ecal2dwindow_idxoffset = 2;
   static constexpr unsigned int ecal2dwindow = (2 * (ecal2dwindow_idxoffset) + 1) * (2 * (ecal2dwindow_idxoffset) + 1);
   std::array<float, ecal2dwindow> c_edep_;
+
+  int foundGoodTrack_, trkq_;
+  float trkpt_, trketa_, trkphi_, trkd0_, trkdz_, trkpMode_, trketaMode_, trkphiMode_, trkqoverpModeError_,
+      trkchi2overndf_;
 
 public:
   struct ShowerStruct {
@@ -114,8 +128,15 @@ public:
 };
 
 Run3ScoutingEGammaP4RegressTrainNtupliser::Run3ScoutingEGammaP4RegressTrainNtupliser(const edm::ParameterSet& iConfig)
-    : electronToken_(consumes<std::vector<Run3ScoutingElectron>>(iConfig.getParameter<edm::InputTag>("electrons"))) {
+    : electronToken_(consumes<std::vector<Run3ScoutingElectron>>(iConfig.getParameter<edm::InputTag>("electrons"))),
+      run3SctEle_bestTrkToken_(
+          consumes<edm::ValueMap<int>>(iConfig.getParameter<edm::InputTag>("run3ScoutingElectronBestTrk"))),
+      isMC_(iConfig.getParameter<bool>("isMC")) {
   usesResource("TFileService");
+
+  if (isMC_) {
+    genParticleToken_ = consumes<std::vector<reco::GenParticle>>(iConfig.getParameter<edm::InputTag>("genParticles"));
+  }
 }
 
 Run3ScoutingEGammaP4RegressTrainNtupliser::~Run3ScoutingEGammaP4RegressTrainNtupliser() {}
@@ -123,6 +144,13 @@ Run3ScoutingEGammaP4RegressTrainNtupliser::~Run3ScoutingEGammaP4RegressTrainNtup
 void Run3ScoutingEGammaP4RegressTrainNtupliser::beginJob() {
   edm::Service<TFileService> fs;
   tree_ = fs->make<TTree>("Events", "Run3 Scouting Electron Regression Training Ntuple");
+
+  // Branch to store genparticle information for MC only
+  if (isMC_) {
+    tree_->Branch("gen_pt", &gen_pt_, "gen_pt/F");
+    tree_->Branch("gen_eta", &gen_eta_, "gen_eta/F");
+    tree_->Branch("gen_phi", &gen_phi_, "gen_phi/F");
+  }
 
   tree_->Branch("pt", &pt_, "pt/F");
   tree_->Branch("eta", &eta_, "eta/F");
@@ -163,17 +191,60 @@ void Run3ScoutingEGammaP4RegressTrainNtupliser::beginJob() {
   tree_->Branch("e2x5B", &e2x5B_, "e2x5B/F");
 
   tree_->Branch("c_edep", c_edep_.data(), "c_edep[25]/F");
+
+  tree_->Branch("foundGoodTrack", &foundGoodTrack_, "foundGoodTrack/I");
+  tree_->Branch("trkpt", &trkpt_, "trkpt/F");
+  tree_->Branch("trketa", &trketa_, "trketa/F");
+  tree_->Branch("trkphi", &trkphi_, "trkphi/F");
+  tree_->Branch("trkd0", &trkd0_, "trkd0/F");
+  tree_->Branch("trkdz", &trkdz_, "trkdz/F");
+  tree_->Branch("trkq", &trkq_, "trkq/I");
+  tree_->Branch("trkpMode", &trkpMode_, "trkpMode/F");
+  tree_->Branch("trketaMode", &trketaMode_, "trketaMode/F");
+  tree_->Branch("trkphiMode", &trkphiMode_, "trkphiMode/F");
+  tree_->Branch("trkqoverpModeError", &trkqoverpModeError_, "trkqoverpModeError/F");
+  tree_->Branch("trkchi2overndf", &trkchi2overndf_, "trkchi2overndf/F");
 }
 
 void Run3ScoutingEGammaP4RegressTrainNtupliser::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
   edm::Handle<std::vector<Run3ScoutingElectron>> electrons;
   iEvent.getByToken(electronToken_, electrons);
 
-  if (!electrons.isValid())
-    return;
+  edm::Handle<edm::ValueMap<int>> run3SctEle_bestTrks;
+  iEvent.getByToken(run3SctEle_bestTrkToken_, run3SctEle_bestTrks);
 
-  for (const auto& ele : *electrons) {
-    // TODO Add a condition that does exclusive gen matching
+  if (!electrons.isValid())
+    throw cms::Exception("Invalid Product for Run3 Scouting Electrons");
+
+  if (!run3SctEle_bestTrks.isValid())
+    throw cms::Exception("Found Invalid Value Map for Run3 Scouting Electron Best Track Indices");
+
+  for (size_t i = 0; i < electrons->size(); ++i) {
+    const auto& ele = (*electrons)[i];
+
+    // Genmatching for MC
+    if (isMC_) {
+      gen_pt_ = -50.0f;
+      gen_eta_ = -5.0f;
+      gen_phi_ = -5.0f;
+      edm::Handle<std::vector<reco::GenParticle>> genParticles;
+      iEvent.getByToken(genParticleToken_, genParticles);
+      if (genParticles.isValid()) {
+        for (const auto& gen : *genParticles) {
+          if ((abs(gen.pdgId()) == 11) && (gen.fromHardProcessFinalState() || gen.isPromptFinalState())) {
+            math::PtEtaPhiMLorentzVector sctele(ele.pt(), ele.eta(), ele.phi(), ele.m());
+            // Checked for double electron gun the difference between matched gen e's is quite small
+            // In 1797 reco'd scouting electrons in 1000 events, where match not found for 332 for dR < 0.1 and 328 for dR < 0.2
+            if (reco::deltaR(sctele, gen) < 0.1) {
+              gen_pt_ = gen.pt();
+              gen_eta_ = gen.eta();
+              gen_phi_ = gen.phi();
+              break;  // Found a matching gen electron, no need to continue
+            }
+          }
+        }
+      }
+    }
 
     pt_ = ele.pt();
     eta_ = ele.eta();
@@ -217,23 +288,35 @@ void Run3ScoutingEGammaP4RegressTrainNtupliser::analyze(const edm::Event& iEvent
     e2x5T_ = ss.e2x5T;
     e2x5B_ = ss.e2x5B;
 
-    // std::vector<float> trkd0,
-    // std::vector<float> trkdz,
-    // std::vector<float> trkpt,
-    // std::vector<float> trketa,
-    // std::vector<float> trkphi,
-    // std::vector<float> trkpMode,
-    // std::vector<float> trketaMode,
-    // std::vector<float> trkphiMode,
-    // std::vector<float> trkqoverpModeError,
-    // std::vector<float> trkchi2overndf,
-    // std::vector<int> trkcharge,
-    // uint32_t seedId,
-    // uint32_t nClusters,
-    // uint32_t nCrystals,
-    // std::vector<float> energyMatrix,
-    // std::vector<uint32_t> detIds,
-    // std::vector<float> timingMatrix,
+    edm::Ref<std::vector<Run3ScoutingElectron>> eleRef(electrons, i);
+    int bestTrkIdx = (*run3SctEle_bestTrks)[eleRef];
+    if (bestTrkIdx != -1) {
+      foundGoodTrack_ = 1;
+      trkpt_ = ele.trkpt()[bestTrkIdx];
+      trketa_ = ele.trketa()[bestTrkIdx];
+      trkphi_ = ele.trkphi()[bestTrkIdx];
+      trkd0_ = ele.trkd0()[bestTrkIdx];
+      trkdz_ = ele.trkdz()[bestTrkIdx];
+      trkq_ = ele.trkcharge()[bestTrkIdx];
+      trkpMode_ = ele.trkpMode()[bestTrkIdx];
+      trketaMode_ = ele.trketaMode()[bestTrkIdx];
+      trkphiMode_ = ele.trkphiMode()[bestTrkIdx];
+      trkqoverpModeError_ = ele.trkqoverpModeError()[bestTrkIdx];
+      trkchi2overndf_ = ele.trkchi2overndf()[bestTrkIdx];
+    } else {
+      foundGoodTrack_ = 0;
+      trkpt_ = -50.0f;
+      trketa_ = -5.0f;
+      trkphi_ = -5.0f;
+      trkd0_ = 0.0f;
+      trkdz_ = -50.0f;
+      trkq_ = -3.0f;
+      trkpMode_ = -500.0f;
+      trketaMode_ = -5.0f;
+      trkphiMode_ = -5.0f;
+      trkqoverpModeError_ = -1.0f;
+      trkchi2overndf_ = -5.0f;
+    }
 
     tree_->Fill();
   }
